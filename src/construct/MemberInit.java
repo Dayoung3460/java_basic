520258add61220ed8b123b2519ffde1ea59b4aed
dayoung <kimdayoung771@gmail.com>
2024-07-04 19:28:33 +0900

    default constructor

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -5,6 +5,11 @@ public class MemberInit {
     int age;
     int grade;
 
+    // 파라미터 없는 생성자는 기본생성자가 자동으로 생성됨
+//    MemberInit() {
+//
+//    }
+
     void initMember(String name, int age, int grade) {
         this.name = name;
         this.age = age;
